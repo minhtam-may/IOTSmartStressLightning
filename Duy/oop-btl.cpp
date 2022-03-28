542e7df0b2de879632e2bbc8c7d221c151a08f0d
dtd217 <duydong217@gmail.com>
2022-03-28 15:03:58 +0700

    update2

--- Duy/oop-btl.cpp
+++ Duy/oop-btl.cpp
@@ -3,73 +3,76 @@
 
 using namespace std;
 
-class Den{
+class Den {
 	protected:
-		string trangThai;
-		float tieuThuNL, congSuat, phamVi, thoiGianSang;
-		int cuongDoSang;
+		string trang_Thai;
+		float tieu_Thu_Nang_Luong, cong_Suat, pham_Vi;
+		int thoi_Gian_Sang, cuong_Do_Sang;
 	public:
 		Den(){
-			
+			trang_Thai = "Binh thuong";
+			cong_Suat = 150;
+			pham_Vi = 150;
+			thoi_Gian_Sang = 0;
+			tieu_Thu_Nang_Luong = 0;		
 		}
 		
-		void Trangthai() {
-			cout<<"Nhap tinh trang cua den h(hong)/b(binh thuong): ";
-			char c; cin>>c;
-			if (c == 'b') {
-				cout<<"Den o trang thai binh thuong"<<endl;
-			}
-			else cout<<"Den dang bi hong, can thay ngay lap tuc!"<<endl;
+		void set_Trang_Thai(string trang_Thai_Moi) {
+			this->trang_Thai = trang_Thai_Moi;
 		}
 		
-		void setThoigiansang(float thoiGianSang) {
-			this->thoiGianSang = thoiGianSang;
+		string get_Trang_Thai(){
+			return trang_Thai;
 		}
 		
-		float getThoigiansang() {
-			return thoiGianSang;
+		void setThoi_Gian_Sang(int thoi_Gian_Sang_Moi) {
+			this->thoi_Gian_Sang = thoi_Gian_Sang_Moi;
 		}
 		
-		void setCongsuat(float congSuat) {
-			this->congSuat = congSuat;
+		int get_Thoi_Gian_Sang() {
+			return thoi_Gian_Sang;
 		}
 		
-		float getCongsuat() {
-			return congSuat;
+		void set_Cong_Suat(float cong_Suat_Moi) {
+			this->cong_Suat = cong_Suat_Moi;
 		}
 		
-		void setPhamvi(float phamVi) {
-			this->phamVi = phamVi;
+		float get_Cong_Suat() {
+			return cong_Suat;
 		}
 		
-		float getPhamvi() {
-			return phamVi;
+		void set_Pham_Vi(float pham_Vi_Moi) {
+			this->pham_Vi = pham_Vi_Moi;
 		}
 		
-		void setCuongdosang(int cuongDoSang) {
-			this->cuongDoSang = cuongDoSang;
+		float get_Pham_Vi() {
+			return pham_Vi;
 		}
 		
-		int getCuongdosang() {
-			return cuongDoSang;
+		void set_Cuong_Do_Sang(int cuong_Do_Sang_Moi) {
+			this->cuong_Do_Sang = cuong_Do_Sang_Moi;
 		}
 		
-		float getTieuthuNL () {
-			this->tieuThuNL = this->congSuat * this->thoiGianSang;
-			return this->tieuThuNL;
+		int get_Cuong_Do_Sang() {
+			return cuong_Do_Sang;
+		}
+		
+		float get_Tieu_Thu_Nang_Luong() {
+			this->tieu_Thu_Nang_Luong = this->cong_Suat * this->thoi_Gian_Sang;
+			return this->tieu_Thu_Nang_Luong;
 		}
 };
 
 int main() {
 	Den d;
-	d.Trangthai();
-	d.setThoigiansang(10);
-	d.setCongsuat(200);	
-	d.setPhamvi(10);
-	d.setCuongdosang(250);	
-	cout<<"Cong suat cua den: "<<d.getCongsuat()<<" kW."<<endl;
-	cout<<"Thoi gian sang cua den: "<<d.getThoigiansang()<<" h."<<endl;
-	cout<<"Pham vi chieu sang cua den: "<<d.getPhamvi()<<" m."<<endl;
-	cout<<"Cuong do sang cua den: "<<d.getCuongdosang()<<" cd."<<endl;
-	cout<<"Muc tieu thu nang luong cua den: "<<d.getTieuthuNL()<<" kW/h."<<endl;
+	d.set_Trang_Thai("h");
+	d.setThoi_Gian_Sang(10);
+	d.set_Cong_Suat(200);	
+	d.set_Pham_Vi(10);
+	d.set_Cuong_Do_Sang(250);	
+	cout<<"Cong suat cua den: "<<d.get_Cong_Suat()<<" kW"<<endl;
+	cout<<"Thoi gian sang cua den: "<<d.get_Thoi_Gian_Sang()<<" h"<<endl;
+	cout<<"Pham vi chieu sang cua den: "<<d.get_Pham_Vi()<<" m"<<endl;
+	cout<<"Cuong do sang cua den: "<<d.get_Cuong_Do_Sang()<<" cd"<<endl;
+	cout<<"Muc tieu thu nang luong cua den: "<<d.get_Tieu_Thu_Nang_Luong()<<" kW/h"<<endl;
 }
